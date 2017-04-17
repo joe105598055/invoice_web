cc8574e65f0807b73729695b089c8e4c0b2dc5e3
joe <qwert30830@gmail.com>
2017-04-17 13:37:44 +0800

    add 3 UT

--- src/test/java/tw/teddysoft/bdd/domain/invoice/CompanyTranslatorTest.java
+++ src/test/java/tw/teddysoft/bdd/domain/invoice/CompanyTranslatorTest.java
@@ -20,4 +20,23 @@ public class CompanyTranslatorTest {
     public void getCompanyName_By_vatID() throws IOException {
         assertThat(CompanyTranslator.getCompanyName("53909614")).isEqualTo("泰迪軟體科技有限公司");
     }
+
+    @Test
+    public void enter_NotExist_Company() throws IOException {
+        assertThat(CompanyTranslator.getVatID("王小明有限公司")).isEqualTo("查無此統一編號");
+    }
+
+    @Test
+    public void enter_NotExist_VatID() throws IOException {
+        assertThat(CompanyTranslator.getCompanyName("12345678")).isEqualTo("查無此公司");
+    }
+
+    @Test
+    public void enter_ErrorVatId_Format() throws IOException {
+        assertThat(CompanyTranslator.getCompanyName("1234567")).isEqualTo("統一編號格式錯誤");
+    }
+
+
+
+
 }
